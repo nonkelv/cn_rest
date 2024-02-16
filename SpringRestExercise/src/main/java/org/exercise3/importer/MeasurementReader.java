6ae41f17b75eea23a6fa95497c05e2824a2e6b48
Jo Dotremont <jo.dotremont@gmail.com>
2024-02-16 15:39:26 +0100

    add a workaround for the measurements.txt file not being (found) in the executable jar.

--- SpringRestExercise/src/main/java/org/exercise3/importer/MeasurementReader.java
+++ SpringRestExercise/src/main/java/org/exercise3/importer/MeasurementReader.java
@@ -1,5 +1,6 @@
 package org.exercise3.importer;
 
+import org.aspectj.util.FileUtil;
 import org.exercise3.model.CarRegistration;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -21,15 +22,23 @@ public class MeasurementReader {
     public List<CarRegistration> readMeasurementsFromFile(String fileName) throws IOException {
         List<CarRegistration> carRegistrations = new ArrayList<>();
 
-        File file = ResourceUtils.getFile("classpath:" + fileName);
-        try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
-            String line = reader.readLine();
-            while (line != null) {
-                CarRegistration carRegistration = measurementLineParser.parseMeasurementLine(line);
-                carRegistrations.add(carRegistration);
-                line = reader.readLine();
+        File file = ResourceUtils.getFile(fileName);
+        // I don't seem to get the txt file in the jar, and I'm not sure I even want that,
+        // so to get both working, let's just add the 2 possibilities:
+        // first try to find it next to the jar, and if not found in the resource folder.
+        if (!file.exists())
+            file = ResourceUtils.getFile("classpath:" + fileName);
+        // should split this in a separate method I guess, but it's Friday afternoon and I am getting tired...
+
+        if (file.exists())
+            try (BufferedReader reader = new BufferedReader(new FileReader(file))) {
+                String line = reader.readLine();
+                while (line != null) {
+                    CarRegistration carRegistration = measurementLineParser.parseMeasurementLine(line);
+                    carRegistrations.add(carRegistration);
+                    line = reader.readLine();
+                }
             }
-        }
         return carRegistrations;
     }
 }
